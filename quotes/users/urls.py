40df8783818c8fe641060c3571b25fda02f6e2a1
Oleh Ovchinnikov <112849429+xoka-pro@users.noreply.github.com>
2023-04-23 19:39:52 +0300

    + reset password

--- quotes/users/urls.py
+++ quotes/users/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView,\
+    PasswordResetConfirmView, PasswordResetCompleteView
 
 from .forms import LoginForm
-from .views import RegisterView
+from .views import RegisterView, ResetPasswordView
+
 
 app_name = "users"
 
@@ -12,4 +14,14 @@ urlpatterns = [
     path("signin/", LoginView.as_view(template_name='users/signin.html', authentication_form=LoginForm,
                                       redirect_authenticated_user=True), name='login'),
     path("logout/", LogoutView.as_view(template_name="app_quotes/quotes.html"), name='logout'),
+    path('reset-password/', ResetPasswordView.as_view(), name='password_reset'),
+    path('reset-password/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                          success_url='/users/reset-password/complete/'),
+         name='password_reset_confirm'),
+    path('reset-password/complete/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
--- quotes/users/views.py
+++ quotes/users/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
@@ -29,3 +33,12 @@ class RegisterView(View):
             messages.success(request, f'Вітаю {username}. Ваш аккаунт успішно створено')
             return redirect(to="users:login")
         return render(request, self.template_name, context={"form": form})
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
